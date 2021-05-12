117a2b4a993bc8c433bd9dff39ace8d6e49c7e03
Gage Bachik <gbachik>
2021-05-11 22:19:40 -0700

    productions build fixes

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+module.exports = withNextra({
+  webpack(config) {
+    config.plugins.pop() // remove stork plugin which was added by nextra. Remove when nextra version is bumped!
+    return config
+  },
+  distDir: 'build'
+})
